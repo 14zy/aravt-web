be2480cb72043d3d01c1b9eeb0df3c943e465a3c
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2024-09-14 08:19:06 +0300

    Update aravt.tsx

--- tg-react-main/src/components/aravt.tsx
+++ tg-react-main/src/components/aravt.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../assets/css/aravt.module.css';
 import Preloader from './Preloader'; // Импорт компонента Preloader
 import logo from '../assets/img/img.png'; // Ensure your logo exists at this path
 import MainButton from './MainButton';
+import { getStartParam, StartParam } from '../hooks/userInfo';
 
 const Aravt: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  const [startParam, setStartParam] = useState<StartParam | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
 
   // Функция, вызываемая после полной загрузки прелоадера
   const handleLoaded = () => {
     setIsLoaded(true);
   };
+  
+  useEffect(() => {
+    try {
+      const fetchedStartParam = getStartParam();
+      if (fetchedStartParam) {
+        setStartParam(fetchedStartParam);
+      }
+    } catch (err) {
+      console.error('Error fetching user info:', err);
+      setError('Failed to load user information');
+    }
+  }, []);
 
   return (
     <div className={styles.aravtContainer}>
@@ -31,6 +48,11 @@ const Aravt: React.FC = () => {
             <span className={styles.dot}></span>
             <span className={styles.dot}></span>
           </div>
+          {startParam ? (
+         <p className="welcome-message">{startParam.start_param}</p>
+       ) : (
+         error && <p className="error-message">{error}</p>
+       )}
           {/* Пример использования MainButton */}
           <MainButton text="Next (1/6)" link="/aravt2" />
         </div>
